21c5f39c2dab98928ab694dd0395fa39389bceea
memozir <gizmovap@gmail.com>
2022-11-24 12:36:55 +0300

    safari-fix delete test function

--- main/static/main/js/safari-fix.js
+++ main/static/main/js/safari-fix.js
@@ -1,11 +1,6 @@
 'use strict'
 
-// window.addEventListener('resize', () => {
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//   });
-
-  window.onresize = function(){
-    document.body.height = window.innerHeight;
-  }
-  window.onresize()
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
