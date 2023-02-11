07600714d3fab7e306cebed9b59e5b5d54b54f29
memozir <gizmovap@gmail.com>
2023-02-11 14:07:47 +0300

    integrated service find form in template

--- main/views.py
+++ main/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News, Services
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import ServiceFindFrom
+
+
 def index(request):
     news = News.objects.all()[:4]
     services = Services.objects.all()
     services_first = services[:8]
     services_second = services[8:]
+    
+    form = ServiceFindFrom()
+    
     context = {
         'news': news,
         'services': services_first,
-        'services_second': services_second
+        'services_second': services_second,
+        'form': form
     }
     
     return render(request, 'main/index.html', context=context)
+
+
+def service_find(request):
+    
+    if request.method == 'POST':
+        sum = request.POST.get('sum')
+        
+        return HttpResponse(sum)
