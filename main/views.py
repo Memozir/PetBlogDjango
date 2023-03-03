0782b66fb05b87236414680849a6c2a856386aa7
Memozir <gizmovap@gmail.com>
2023-03-03 15:38:45 +0300

    form filtering feature, not finished yet

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import News, Services
 from django.http import HttpResponse
+from django.db.models.functions import Cast
+from django.db.models import IntegerField
 
 from .forms import ServiceFindFrom
 
@@ -26,6 +28,23 @@ def index(request):
 def service_find(request):
     
     if request.method == 'POST':
-        sum = request.POST.get('sum')
-        
-        return HttpResponse(sum)
+        form = ServiceFindFrom(request.POST)
+
+        if form.is_valid():
+            sum_value = int(form.cleaned_data['sum'])
+
+            services = Services.objects.annotate(
+                money_limit_int__lt=Cast('money_limit', output_field=IntegerField())
+            ).filter(
+                money_limit_int__lt=sum_value)
+
+            context = {
+                'news': None,
+                'services': services,
+                'serevices_second': None,
+                'form': form
+            }
+
+        return render(request, template_name='main/index.html', context=context)
+    else:
+        redirect('main/index.html')
